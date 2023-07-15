336f4b4b3b4dc41d58481b030eba8580c83e712a
Daniel Muñoz <daniel20025febrero29@gmail.com>
2023-07-15 11:43:59 -0500

    Create components with initial interface easy

---
+++ src/Layouts/Dashboard/dashboard.jsx
@@ -0,0 +1,16 @@
+import Header from "../Header/header";
+import Footer from "../Footer/footer";
+
+function Dashboard() {
+
+    return (
+        <>
+            <Header />
+            <h1 className="text-3xl font-bold underline"> Inicio </h1>
+            <Footer/>
+        </>
+    );
+}
+
+
+export default Dashboard;
---
+++ src/Layouts/Header/header.jsx
@@ -0,0 +1,28 @@
+function Header () {
+
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            Inicio
+                        </li>
+                        <li>
+                            Gestion Usuarios
+                        </li>
+                        <li>
+                            Gestion Cuentas
+                        </li>
+                        <li>
+                            Configuración
+                        </li>                       
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
+
+
+export default Header;
