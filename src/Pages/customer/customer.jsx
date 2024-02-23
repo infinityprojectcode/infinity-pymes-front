02340eba403d2fdd34d0abc2b5f12e5844f02076
= <daniel20025febrero29@gmail.com>
2024-02-23 00:15:24 -0500

    Clients list

--- src/Pages/customer/customer.jsx
+++ src/Pages/customer/customer.jsx
@@ -1,12 +1,15 @@
 import Sidebar from "../../Layouts/sidebar/sidebar";
 import PageTemplate from "../../Layouts/page/page-template";
+import ListCustomers from "../../components/customers/list-customers";
 
 export default function customer() {
   return (
     <div className="w-full h-screen flex">
       <Sidebar />
       <PageTemplate title='Clientes'>
-
+        <div className='w-full h-full pt-10 p-8'>
+          <ListCustomers />
+        </div>
       </PageTemplate>
     </div>
   )
