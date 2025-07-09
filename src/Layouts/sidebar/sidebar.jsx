c37d61fb1537b398daa8eda92b4dbf7304f91450
EmmaHQ <hernandezehq@gmail.com>
2025-07-09 00:06:52 -0500

    create billing-icon, transform slidebar las the mockup, set the routes

--- src/Layouts/sidebar/sidebar.jsx
+++ src/Layouts/sidebar/sidebar.jsx
@@ -1,56 +1,56 @@
 import { Link } from "react-router-dom";
 import { routes } from "../../routes/routes";
+import LogoutIcon from "../../assets/icons/logout-icon";
 
 export default function Sidebar() {
   return (
     <div className="flex flex-col relative top-0 left-0 h-screen w-1/6 bg-100">
       <div className="flex flex-col h-full justify-start">
-        <div className="flex flex-col justify-center text-center p-8">
-          <div>
-            <Link to="/user-login">
-              <button className="text-200">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-user-circle"
-                  width="84"
-                  height="84"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                  <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                  <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
-                </svg>
-              </button>
-            </Link>
+        <div className="flex flex-col text-center p-4 gap-2 pb-2">
+          <div className="flex flex-col w-fit mx-auto">
+            <h1 className="text-2xl font-bold text-100 whitespace-nowrap">
+              Infinity Pymes
+            </h1>
+            <h2 className="text-sm text-200 mt-1 whitespace-nowrap">
+              Gestión administrativa
+            </h2>
           </div>
-          <h1 className="text-2xl font-base text-100 font-bold ">@User</h1>
+
+          <div className="flex items-center gap-4 mt-3">
+            <img
+              src="/ruta/de/imagen.jpg"
+              alt="Foto de perfil"
+              className="w-12 h-12 rounded-full border border-gray-300"
+            />
+            <div className="flex flex-col items-start">
+              <span className="text-100 font-semibold">Usuario User</span>
+              <span className="text-sm text-200">Administrador</span>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col h-full gap-4">
+        <div className="w-full border-t border-gray-300 my-4" />
+
+        <div className="flex flex-col h-full">
           {routes.map((item, index) => (
             <Link key={index} className="text-200" to={item.route}>
-              <div className="p-4">
-                <div
-                  key={item.id}
-                  className="w-full h-12 p-4 rounded-lg flex gap-10 flex-row justify-start items-center hover:bg-white hover:cursor-pointer hover:text-black font-semibold"
-                >
-                  <div className="flex flex-row justify-start items-center">
-                    {item.icon}
-                  </div>
-                  <span>{item.name}</span>
+              <div
+                key={item.id}
+                className="w-4/5 h-9 px-2 my-1 mx-auto rounded-lg flex gap-4 flex-row justify-start items-center hover:bg-white hover:cursor-pointer hover:text-black font-semibold"
+              >
+                <div className="flex flex-row justify-start items-center">
+                  {item.icon}
                 </div>
+                <span>{item.name}</span>
               </div>
             </Link>
           ))}
         </div>
       </div>
-      <div className="flex flex-col justify-end text-center p-8">
-        <p className="text-200">&copy; 2025 Infinit Pymes</p>
+      <div className="p-4 border-t border-gray-300 mt-4">
+        <button className="w-full h-12 p-4 rounded-lg flex gap-2 flex-row justify-start items-center hover:bg-white hover:cursor-pointer hover:text-black font-semibold text-200">
+          <LogoutIcon />
+          <span>Cerrar sesión</span>
+        </button>
       </div>
     </div>
   );
