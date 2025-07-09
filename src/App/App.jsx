20175b5dae3126206959f4e9d8f26c8e1f28152c
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-07-09 11:55:16 -0500

    Fix import paths and reorder imports in App.jsx
    Corrected the import path for App in main.jsx to use lowercase 'app'. Reordered and cleaned up imports in App.jsx for consistency and to resolve potential import issues.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import './App.css'
 import { createBrowserRouter, createHashRouter, Route, RouterProvider, HashRouter } from 'react-router-dom'
-import Dashboard from '@pages/dashboard/dashboard'
 import ProtectedRoutes from '@components/auth/protectedRoutes/ProtectedRoutes'
-import Home from '@pages/home/home'
-import Login from '@components/login/login'
-import Signup from '@components/signup/signup'
-import Inventory from '@pages/inventory/inventory'
-import Customer from '@pages/customer/customer'
 import UserLogin from '@pages/userlogin/user-login'
+import Inventory from '@pages/inventory/inventory'
+import Dashboard from '@pages/dashboard/dashboard'
+import Customer from '@pages/customer/customer'
+import Signup from '@components/signup/signup'
+import Login from '@components/login/login'
+import Home from '@pages/home/home'
 
 function App() {
   const routes = createHashRouter([
