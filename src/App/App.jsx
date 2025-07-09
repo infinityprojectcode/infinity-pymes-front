6055d4ae132fc66e0624d08e359615912994a90d
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-07-09 17:13:40 -0500

    Wrap App with AuthProvider and clean up imports
    The App component is now wrapped with AuthProvider in main.jsx to provide authentication context. Also cleaned up imports and removed commented code and unused variables in App.jsx.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,46 +1,7 @@
-/* eslint-disable no-unused-vars */
 import Router from '@routes/root';
-import './App.css'
+import '@/App/App.css';
 
 function App() {
-  // const routes = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Login />,
-  //   },
-  //   {
-  //     path: "/signup",
-  //     element: <Signup />,
-  //   },
-  //   {
-  //     path: "/dashboard",
-  //     element: <Dashboard />,
-  //   },
-  //   {
-  //     path: "/home",
-  //     element: <Home />,
-  //   },
-  //   {
-  //     path: "/inventory",
-  //     element: <Inventory />,
-  //   },
-  //   {
-  //     path: "/customer",
-  //     element: <Customer />,
-  //   },
-  //   {
-  //     path: "/user-login",
-  //     element: <UserLogin />,
-  //   },
-  //   {
-  //     path: '/',
-      // element: <ProtectedRoutes />,
-  //     children: [
-  //       { path: '', element: '' },
-  //     ]
-  //   }
-  // ])
-
   return (
     <>
       <Router />
