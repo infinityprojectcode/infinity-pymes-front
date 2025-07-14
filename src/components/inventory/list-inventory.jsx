18637e95832cc77ab8cfdaa2838309c199550eac
Deymer Ospina <deymer.ospina@gmail.com>
2025-07-13 20:18:28 -0500

    Inventory Design Recovery

--- src/components/inventory/list-inventory.jsx
+++ src/components/inventory/list-inventory.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import bebidas from "@assets/images/bebida.webp";
 import { v4 as crypto } from "uuid";
-import AddInventory from './modal-add-inventory/modal-add-inventory.jsx';
+import AddInventory from "./modal-add-inventory/modal-add-inventory.jsx";
 
 export default function ListInventory() {
   const categoriesList = [
@@ -28,14 +28,14 @@ export default function ListInventory() {
       name: "Licor",
       category: "Licores",
       price: "5000",
-      stock: 12,
+      stock: 3,
     },
     {
       id: crypto(),
       name: "Gaseosa",
       category: "Gaseosas",
       price: "5000",
-      stock: 15,
+      stock: 0,
     },
   ];
 
@@ -54,6 +54,12 @@ export default function ListInventory() {
     setPriceProduct("");
     setStockProduct("");
   }
+
+  const getStatus = (stock) => {
+    if (stock == 0) return { name: "Agotado", color: "bg-red-500" };
+    if (stock <= 5) return { name: "Stock bajo", color: "bg-yellow-500" };
+    return { name: "En stock", color: "bg-green-500" };
+  };
 
   const handleAddProduct = () => {
     setListProduct((prev) => [
@@ -107,89 +113,131 @@ export default function ListInventory() {
   }, [listProduct]);
 
   return (
-    <div>
-      <button
-        className="bg-blue-500 rounded-lg p-2 text-white"
-        onClick={()=>setModalIsOpen(true)}
-      >
-        Añadir nuevo
-      </button>   
-        <div className="w-full h-full pt-5">
-          <ul className="[&>li>h1]:font-black flex flex-col gap-4 [&>li]:rounded-lg [&>li]:p-4">
-            {listProduct.map((item, index) => (
-              <li key={index} className="bg-200">
-                <div className="flex flex-row items-center justify-between gap-4">
-                  <div className="flex gap-2">
-                    <h1 className="text-100">Producto:</h1>
-                    <span className="text-200">{item.name}</span>
-                  </div>
-                  <div className="flex gap-2">
-                    <h1 className="text-100">Categoría:</h1>
-                    <span className="text-200">{item.category}</span>
-                  </div>
-                  <div className="flex gap-2">
-                    <h1 className="text-100">Precio:</h1>
-                    <span className="text-200">${item.price}</span>
-                  </div>
-                  <div className="flex gap-2">
-                    <h1 className="text-100">Cantidad:</h1>
-                    <span className="text-200">{item.stock}</span>
-                  </div>
-                  <div className="flex gap-2">
-                    <button
-                      onClick={() => handleEditProduct(item.id)}
-                      className="bg-blue-500 rounded-lg p-2 text-white"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="icon icon-tabler icon-tabler-pencil"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-                        <path d="M13.5 6.5l4 4" />
-                      </svg>
-                    </button>
-                    <button
-                      onClick={() => handleDeleteProduct(item.id)}
-                      className="bg-red-500 rounded-lg p-2 text-white"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="icon icon-tabler icon-tabler-trash"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M4 7l16 0" />
-                        <path d="M10 11l0 6" />
-                        <path d="M14 11l0 6" />
-                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+    <>
+      <div className="flex flex-col">
+        <div className="flex justify-between items-start">
+          <div className="flex flex-col text-white">
+            <h1 className="text-3xl font-bold mb-1">Inventario</h1>
+            <p className="text-gray-400 mb-6">
+              Gestione su inventario de productos y los niveles de stock.
+            </p>
+          </div>
+          <button
+            onClick={() => setModalIsOpen(true)}
+            className="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold p-2 rounded-lg transition duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5v14" />
+            </svg>
+            <span>Agregar nuevo producto</span>
+          </button>
         </div>
+        <div className="relative w-full max-w-sm mb-4">
+          <input
+            type="text"
+            placeholder="Buscar productos..."
+            className="w-full pl-10 pr-4 py-2 text-white rounded-lg bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white focus:border-white text-sm"
+          />
+          <svg
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-4.35-4.35M16.65 16.65A7.5 7.5 0 1116.65 2a7.5 7.5 0 010 15z"
+            />
+          </svg>
+        </div>
+        <div className="w-full overflow-x-auto rounded-lg border border-gray-700  bg-gray-900 text-white p-3">
+          <h1 className="text-2xl font-bold mb-1 ">Products(5)</h1>
+          {/* Encabezado */}
+          <div className="grid grid-cols-6 text-sm font-semibold px-4 py-3">
+            <div>Nombre del producto</div>
+            <div>Categoría</div>
+            <div>Precio</div>
+            <div>Cantidad</div>
+            <div>Estado</div>
+            <div>Acciones</div>
+          </div>
 
-            <AddInventory isOpen={modalIsOpen} onClose={()=>setModalIsOpen(false)}></AddInventory>
+          {/* Contenido dinámico */}
+          {listProduct.map((item) => {
+            const auxiliar = getStatus(item.stock);
+            return (
+              <div
+                key={item.id}
+                className="grid grid-cols-6 items-center text-white px-4 py-3 border-t border-gray-700 hover:bg-gray-800 transition"
+              >
+                <div className="truncate">{item.name}</div>
+                <div>{item.category}</div>
+                <div>${item.price}</div>
+                <div>{item.stock}</div>
+                <div
+                  className={`${auxiliar.color} w-fit px-2 text-white text-center font-semibold rounded-lg`}
+                >
+                  {auxiliar.name}
+                </div>
+                <div className="flex gap-2">
+                  <button
+                    onClick={() => handleEditProduct(item.id)}
+                    className=" text-white rounded-md text-sm"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                    >
+                      <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                      <path d="m15 5 4 4" />
+                    </svg>
+                  </button>
+                  <button
+                    onClick={() => handleDeletProduct(item.id)}
+                    className="rounded-md text-sm"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="text-red-500"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                    >
+                      <path d="M10 11v6" />
+                      <path d="M14 11v6" />
+                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                      <path d="M3 6h18" />
+                      <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
 
-    </div>
+      <AddInventory
+        isOpen={modalIsOpen}
+        onClose={() => setModalIsOpen(false)}
+      ></AddInventory>
+    </>
   );
 }
