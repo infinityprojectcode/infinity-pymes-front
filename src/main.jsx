eb9839ca84901722021db210c5ae506486284a2b
Deymer Ospina <deymer.ospina@gmail.com>
2025-08-13 05:35:54 -0500

    Fix error in modal integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import AppProvider from "./context/app/context-provider";
 import App from "./app/app";
+import Modal from "react-modal";
 import "./index.css";
+
+Modal.setAppElement("#root");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
