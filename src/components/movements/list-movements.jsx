6f7e79f850080cee6314ad7706a60aa06c5730e4
Deymer Ospina <deymer.ospina@gmail.com>
2025-08-29 04:38:02 -0500

    Note update

--- src/components/movements/list-movements.jsx
+++ src/components/movements/list-movements.jsx
@@ -12,7 +12,7 @@ import {
 } from "lucide-react";
 import axios from "axios";
 
-// NOTA: Revisar luego el flujo de datos en auditoria para ver si se hace de un dia o por completo, también revisar el cierre diario para crear un saldo inicial con el cual iniciar la caja, actualmente inicia en 0 pero puede que el local no sea nuevo y tengan que añadir el dinero actual en efectivo, también revisar si todo se esta manejando solo con efectivo en el flujo de datos, porque al final aunque debemos saber el movimiento de todo el dinero, al final el cierre de caja se hace con el dinero en efectivo.
+// NOTA: Revisar luego el flujo de datos en auditoria para ver si se hace de un dia o por completo, también revisar el cierre diario para crear un saldo inicial con el cual iniciar la caja, actualmente inicia en 0 pero puede que el local no sea nuevo y tengan que añadir el dinero actual en efectivo, también revisar si todo se esta manejando solo con efectivo en el flujo de datos, porque al final aunque debemos saber el movimiento de todo el dinero, al final el cierre de caja se hace con el dinero en efectivo. Revisar la tabla de resumen por método de pago, debería solamente mostrar el total del dinero ganado dependiendo del método de pago y no hacer cálculos de dinero gastado, ademas se podría mejorar agregando 3 columnas mas, para mostrar los ingresos, gastos y total que se ha hecho con un método de pago.
 
 export default function ListMovements() {
   const context = useAppContext();
